01adf89b8339ef055ae333f0311f3bb2ed1d8b03
vbungay <113390913+vbungay@users.noreply.github.com>
2023-05-18 17:01:23 -0400

    chart

--- src/components/charts/Charts.jsx
+++ src/components/charts/Charts.jsx
@@ -1,12 +1,73 @@
 import React from 'react'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './Charts.scss'
 
 const Charts = () => {
-  return (
-    <div className="chart">
-        Chart
-    </div>
-  )
+    const data = [
+        {
+            month: 'January',
+            sales: 3000,
+        },
+        {
+            month: 'February',
+            sales: 4000,
+        },
+        {
+            month: 'March',
+            sales: 2500,
+        },
+        {
+            month: 'January',
+            sales: 3000,
+        },
+        {
+            month: 'February',
+            sales: 4000,
+        },
+        {
+            month: 'March',
+            sales: 2500,
+        },
+        {
+            month: 'January',
+            sales: 3000,
+        },
+        {
+            month: 'February',
+            sales: 4000,
+        },
+        {
+            month: 'March',
+            sales: 2500,
+        },
+        {
+            month: 'January',
+            sales: 3000,
+        },
+        {
+            month: 'February',
+            sales: 4000,
+        },
+        {
+            month: 'March',
+            sales: 2500,
+        },
+    ];
+
+    return (
+        <div className="chart">
+            <h3 className="title">
+                Quarterly Sales
+            </h3>
+            <ResponsiveContainer width="100%" aspect={4 / 1}>
+                <LineChart data={data}>
+                    <XAxis dataKey="month" stroke="#6c757d" />
+                    <YAxis />
+                    <Line type="mono" dataKey="sales" />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    )
 }
 
 export default Charts
